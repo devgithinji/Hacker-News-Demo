db695bbab45192c8d8c135128e5cbf39a9578bac
dennis <wakahiad@gmail.com>
2022-06-12 14:18:20 +0300

    final commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Hacker news App
+
+Click [here](https://hacker-news-app-ke.netlify.app) to visit application
+
+Dennis Githinji 👍
--- src/context.js
+++ src/context.js
@@ -1,26 +1,59 @@
-import React, { useContext, useEffect, useReducer } from 'react'
+import React, {useContext, useEffect, useReducer} from 'react'
 
 import {
-  SET_LOADING,
-  SET_STORIES,
-  REMOVE_STORY,
-  HANDLE_PAGE,
-  HANDLE_SEARCH,
+    SET_LOADING, SET_STORIES, REMOVE_STORY, HANDLE_PAGE, HANDLE_SEARCH,
 } from './actions'
 import reducer from './reducer'
+import axios from "axios";
 
 const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?'
 
-const initialState = {}
+const initialState = {
+    isLoading: true,
+    hits: [],
+    query: 'react',
+    page: 0,
+    nbPages: 0
+}
 
 const AppContext = React.createContext()
 
-const AppProvider = ({ children }) => {
-  return <AppContext.Provider value='hello'>{children}</AppContext.Provider>
+const AppProvider = ({children}) => {
+    const [state, dispatch] = useReducer(reducer, initialState);
+
+    const fetchStories = async (url) => {
+        dispatch({type: SET_LOADING})
+        try {
+            const {data} = await axios.get(url);
+            dispatch({type: SET_STORIES, payload: {hits: data.hits, nbPages: data.nbPages}})
+        } catch (e) {
+            console.log(e)
+        }
+    }
+
+
+    const removeStory = (id) => {
+        dispatch({type: REMOVE_STORY, payload: id})
+    }
+
+    const handleSearch = (query) => {
+        dispatch({type: HANDLE_SEARCH, payload: query})
+    }
+
+    const handlePage = (value) => {
+        dispatch({type: HANDLE_PAGE, payload: value})
+    }
+
+    useEffect(() => {
+        fetchStories(`${API_ENDPOINT}query=${state.query}&page=${state.page}`)
+    }, [state.query, state.page])
+
+
+    return <AppContext.Provider value={{...state, removeStory, handleSearch, handlePage}}>{children}</AppContext.Provider>
 }
 // make sure use
 export const useGlobalContext = () => {
-  return useContext(AppContext)
+    return useContext(AppContext)
 }
 
-export { AppContext, AppProvider }
+export {AppContext, AppProvider}
